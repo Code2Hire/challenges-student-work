9d46331f21e16d0a26fc75ea70e9482640ac0c54
tatatatabs <tabithalessthanthree@gmail.com>
2015-08-26 22:22:23 +0000

    stuff

--- week_4/Day_2/01_medium.php
+++ week_4/Day_2/01_medium.php
@@ -31,13 +31,13 @@
          *
          * Perform the following tasks:
          *
-         * 1. Create at least one Clothing Object and one Television Object.
-         * 2. Create a shopping cart instance.
+         * 1. Create at least one Clothing Object and one Television Object. DONE
+         * 2. Create a shopping cart instance. DONE
          * 3. Add the two products to the cart.
-         * 4. Print out the description of the cart.
+         * 4. Print out the description of the cart. DONE
          * 5. Print out the total price of the cart.
          * 6. Remove the Clothing object from your cart.
-         * 7. FInd the product in the cart with the name of your Television Object.
+         * 7. Find the product in the cart with the name of your Television Object.
          * 8. Pass your ShoppingCart into the ItemDescriber outputDescription method from Exercise 4 and see
          * how it will also output the description of your cart, just like it did your individual products
          */
@@ -59,54 +59,16 @@
             $this->price = $price;
           } 
           
+          public function getPrice() { 
+            return $this->price = $price;
+          } 
+          
           public function provideDescription() {
             return $this->provideDescriptionForProductType();
           }
           
           abstract public function provideDescriptionForProductType();
-          
-          public function getName() { 
-            if (!empty($this->name)) { 
-              return $this->name;
-            } 
-            else { 
-              throw new Exception('Empty value found');
-            } 
-          } 
-          
-          public function getBrand() { 
-            if (!empty($this->brand)) { 
-              return $this->brand;
-            } 
-            else { 
-              throw new Exception('Empty value found');
-            } 
-          } 
-          
-          public function getPrice() { 
-            if (!empty($this->price)) { 
-              
-              
-              if(is_numeric($this->price)) {
-              return $this->price;
-            } 
-            else { 
-              throw new Exception('Not a valid number');
-            }
-            
-            } 
-            else { 
-              throw new Exception('Empty value found');
-            } 
-          }
         } 
-        
-        class CloudMovieFile implements Describable { 
-          
-          public function provideDescription() { 
-            return "This is a cloud movie.";
-          }
-        }
         
         class ItemDescriber { 
           public function outputDescription($item) { 
@@ -123,16 +85,20 @@
           } 
           
           public function addProduct($product) { 
-            if($item instanceof Product) { 
-              return $this->item;
+            array_push($coolProducts, $product);
+            
+            if($product instanceof Product) { 
+              return $this->product;
             } 
             throw new Exception("This item is not a valid product.");
           } 
           
           public function removeOne($product) { 
+          
           } 
           
           public function removeAll($product) { 
+            
           } 
           
           public function getTotalPrice() { 
@@ -141,7 +107,7 @@
           public function getAllProducts() { 
           } 
           
-          public function findProductByName() { 
+          public function findProductByName($name) { 
           } 
           
           
@@ -152,47 +118,6 @@
           protected $type;
           protected $gender;
           
-          public function getColor() { 
-            if (!empty($this-color)) { 
-              
-              if (in_array($this->color, array("red", "blue", "green", "black", "white", "yellow"))) { 
-              return $this->color; 
-            } 
-            else { 
-              throw new Exception('Not a valid color');
-            }
-            } 
-            else { 
-              throw new Exception('Empty value found');
-            } 
-          } 
-          
-          public function getType() { 
-            if (!empty($this->type)) {
-              return $this->type;
-            } 
-            else { 
-              throw new Exception('Empty value found');
-            }
-          } 
-          
-          public function getGender() { 
-            if (!empty($this->gender)) { 
-              return $this->gender;
-            } 
-            else {
-              throw new Exception('Empty value found');
-            } 
-          } 
-          
-          public function getSize() { 
-            if (!empty($this->size)) { 
-              return $this->size;
-            } 
-            else { 
-              throw new Exception('Empty value found');
-            } 
-          }
           
           public function __construct($name, $brand, $price, $color, $type, $gender, $size) {
             parent::__construct($name, $brand, $price);
@@ -203,31 +128,13 @@
           } 
           
           public function provideDescriptionForProductType() { 
-            return "It is a {$this->getName()} {$this->getBrand()} {$this->getColor()} {$this->getGender()} {$this->getType()} of {$this->getSize()} size.  It costs {$this->getPrice()}.";
+            return "It is a {$this->name} {$this->brand} {$this->color} {$this->gender} {$this->type} of {$this->size} size.  It costs {$this->price}.";
           } 
         } 
          
         class Television extends Product { 
           protected $displayType; 
           protected $size;
-          
-          public function getDisplayType() { 
-            if(!empty($this->displayType)) { 
-              return $this->displayType; 
-            } 
-            else { 
-              throw new Exception('Empty value found');
-            }
-          } 
-          
-          public function getSize() { 
-            if(!empty($this->size)) { 
-              return $this->size;
-            } 
-            else { 
-              throw new Exception('Empty value found');
-            }
-          } 
           
           public function __construct($name, $brand, $price, $displayType, $size) { 
             parent::__construct($name, $brand, $price);
@@ -236,20 +143,25 @@
           } 
           
           public function provideDescriptionForProductType() { 
-            return "This is a {$this->getSize()} {$this->getBrand()} {$this->getDisplayType()} Television. It costs {$this->getPrice()}.";
+            return "This is a {$this->size} {$this->brand} {$this->displayType} Television. It costs {$this->price}.";
           } 
         } 
+        
+        $coolProducts = array();
       
-        $buttonShirt = new Clothing("Button Down Shirt", "J Peterman", "29.88", "red", "Shirt", "Male", "large");
-        $headband = new Clothing("Headband", "Nikee", "9.00", "yellow", "Headgear", "Neutral", "one size fits all");
-        $giantTV = new Television("Giant TV", "Kramerica", "3900.90", "LED", "100 inches");
-        $childTV = new Television("Child Television", "ChildCorp", "299.00", "Plasma", "23x33");
-        $newMovie = new CloudMovieFile("Willy Wonka", "Mel Stuart", "Fantasy");
-        $newThing = new ItemDescriber();
-        $cartOfShopping = new ShoppingCart();
+        $buttonShirt = new Clothing("Button Down Shirt", "J Peterman", "29.88", "red", "Shirt", "Male", "large"); // new instance of clothing
+        $giantTV = new Television("Giant TV", "Kramerica", "3900.90", "LED", "100 inches"); // new instance of televison
+        $cartOfShopping = new ShoppingCart(); // new instance of shopping cart
+        // $newThing = new ItemDescriber(); idk if I still need this i'll find out soon enough
+        
+        echo $buttonShirt->provideDescription(); 
+        echo "<br/>";
+        echo $giantTV->provideDescription();
+        echo "<br/>";
+        echo $cartOfShopping->provideDescription();
         
         
-        $customerItems = array($buttonShirt, $giantTV, $newMovie);
+        /* $customerItems = array($buttonShirt, $giantTV, $newMovie);
         foreach($customerItems as $item) { 
           echo $newThing->outputDescription($item);
         } 
@@ -260,37 +172,7 @@
         catch (Exception $E) { 
           echo $E->getMessage();
         } 
-        
-        try { 
-          echo $buttonShirt->provideDescription();
-          echo "<br/>"; 
-        } 
-        catch (Exception $E) { 
-          echo $E->getMessage();
-        } 
-        
-        try { 
-          echo $headband->provideDescription();
-          echo "<br/>"; 
-        } 
-        catch (Exception $E) { 
-          echo $E->getMessage();
-        } 
-        
-        try { 
-          echo $giantTV->provideDescription(); 
-          echo "<br/>";
-        } 
-        catch (Exception $E) {
-          echo $E->getMessage();
-        } 
-        
-        try { 
-          echo $childTV->provideDescription();
-        } 
-        catch (Exception $E) { 
-          echo $E->getMessage();
-        }
+        */ 
         ?>
     </p>
   </body>
