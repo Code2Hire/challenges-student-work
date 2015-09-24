19d9c3f6379e6cd17bffe06593a725fd57a3ab6d
nathan2294 <nathanael.arreola@gmail.com>
2015-09-24 04:52:05 +0000

    Finally stopped crashing

--- week_8/day_1/03_hard/duel.js
+++ week_8/day_1/03_hard/duel.js
@@ -33,3 +33,85 @@
  * - ???
  * - Profit!
  */
+
+
+
+function rand() { 
+    var num = Math.floor((Math.random() * 10) + 1);
+    return num;
+} 
+
+
+//functions taken from internet to create hexadecimal, I was thinking of doing different predetermined background colors assigened to a number and then randomizing to get said number, but i tought this gave me a wider range of background styles.
+function rg(m, n) {
+    return Math.floor( Math.random() * (n - m + 1) ) + m;
+}
+
+function hex(i) {
+    return i.toString(16);
+}
+
+function randColor() {
+    return '#' + hex(rg(1, 15)) + hex(rg(1, 15)) + hex(rg(1, 15)) +
+        hex(rg(1, 15)) + hex(rg(1, 15)) + hex(rg(1, 15));
+}
+
+var jacksonButton = document.getElementById('andrew-shoot');
+var charlesButton = document.getElementById('charles-shoot');
+var resetButton = document.getElementById('reset');
+
+var jacksonHits = document.getElementById('andrew-numhits');
+var charlesHits = document.getElementById('charles-numhits');
+var jacksonHitsInt = parseInt(jacksonHits.innerHTML, 10);
+var charlesHitsInt = parseInt(charlesHits.innerHTML, 10);
+
+
+var jacksonShots = document.getElementById('andrew-numshots');
+var jacksonShotsInt = parseInt(jacksonShots.innerHTML, 10);
+
+var charlesShots = document.getElementById('charles-numshots');
+var charlesShotsInt = parseInt(charlesShots.innerHTML, 10);
+/* I tried putting my jquery stiff here, I'm still alittle confused on 
+how it works, for some reason the console would tell me that jquery was undefined
+when I did it like this, but when I put on html under the sript tags, it worked fine
+Am i doing it worong here? I understand jQuery is a framework of javascript, so it should be allowed right?
+It's just a little confusing on how to use jquery, when to use it, and how to define properly
+
+jQuery(function ($) {
+    $(document).ready(function(){
+            $("button").click(function(){
+            $("body").css("background-color", randColor());
+        });
+    });
+});   
+*/
+jacksonButton.addEventListener('click', function() {
+    jacksonShots.innerHTML = jacksonShotsInt++;
+    if (rand() > 5) { 
+        jacksonHits.innerHTML = jacksonHitsInt++;
+    } 
+});
+
+charlesButton.addEventListener('click', function() { 
+    charlesShots.innerHTML = charlesShotsInt++;
+    if  (rand() > 5) { 
+        charlesHits.innerHTML = charlesHitsInt++;
+    } 
+});
+
+resetButton.addEventListener('click', function() { 
+    document.getElementById("andrew-numshots").innerHTML = 0; 
+    document.getElementById("andrew-numhits").innerHTML = 0; 
+    document.getElementById("charles-numshots").innerHTML = 0; 
+    document.getElementById("charles-numhits").innerHTML = 0; 
+    
+    //give the actual variable an actual value of 0 not just display zero as done above^^
+    jacksonShotsInt = 0;
+    charlesShotsInt = 0;
+
+   
+var resetButtonCount = document.getElementById('num-resets'); 
+    resetButtonCount.innerHTML = parseInt(resetButtonCount.innerHTML, 10) + 1;
+}); 
+
+
