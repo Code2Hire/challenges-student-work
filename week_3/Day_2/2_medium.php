34c578b90c8822c7c41de6b5dd305ac1655b4da4
Seth Handy <sethhandy39@gmail.com>
2015-10-13 03:23:03 +0000

    updates

--- week_3/Day_2/2_medium.php
+++ week_3/Day_2/2_medium.php
@@ -11,52 +11,55 @@
               Lets bring in the deck code from the past example. (normal)
               create a function that will create a deck of cards, randomize it and then return the deck
              */
-             function createDeck() {
-                    $suits = array ("Clubs", "Diamonds", "Hearts", "Spades");
-                    $faces = array (
-                         "Ace" => 1, "2" => 2,"3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7,
-                         "8" => 8, "9" => 9, "10" => 10, "Jack" => 11, "Queen" => 12, "King" => 13
-            );
-                $deck = array();
-
-            // print here
-           foreach ($suits as $suit) {
-            foreach ($faces as $face => $value) {
-                    $deck[] = array ("face"=>$face, "suit"=>$suit,"value"=> $value);
-    }
-}
-       $shuffled_deck = shuffle($deck);
-        $card = array_shift($deck);
-
+             
+            function shuffle_assoc(&$array) {
+                $keys = array_keys($array);
+                shuffle($keys);
         
-              }
+                foreach($keys as $key) {
+                    $new[$key] = $array[$key];
+                }
+                $array = $new;
+                return true;
+            }
+              function createDeck() {
+                $suits = array ("clubs", "diamonds", "hearts", "spades");
+                $faces = array (
+                    "Ace" => 1, "2" => 2,"3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7,
+                    "8" => 8, "9" => 9, "10" => 10, "Jack" => 11, "Queen" => 12, "King" => 13
+                );
+    
+                $deck = array();
+    
+                foreach($faces as $key=>$face){
+                    foreach($suits as $suit){
+                        $newKey = "$key of $suit";
+                        $deck[$newKey] = $face;       
+                    }
+                }
                 
-              
-
+                shuffle_assoc($deck);
+            
+                return $deck;
+              }
+               $deck = createDeck();
             /*
                 We will now create a function to deal these cards to each user
                 modify this function so that it returns the number of cards specified to the user
                 also, it must modify the deck so that those cards are no longer available to be ditributed
             */
-           
-            function dealCards(&$deck, $number_of_cards) {
-              createDeck();
-              for($i = 0; $i < $number_of_cards; $i++){
-                  $randsuit = rand(0,3);
-                    $randcard = rand(0,12); 
-                         if( isset($setCards[$suit[$randsuit].$card[$randcard]]) ) {
-                                 $i--;
-                                continue;
-              }
-               
-                return $number_of_cards;
-                
+            
+            //deals cards by creating a new deck and returning the slice of that deck specified by number of cards
+           function dealCards($deck, $number_of_cards ) {
+                 $deck = createDeck();
+                return array_slice($deck,0,$number_of_cards);
             }
-            }
-        
-          $player = dealCards($deck,4);
-          echo $player;
-          
+            
+            $player = dealCards($deck,10); // player should now have 10 random cards in his hand
+            var_dump($player);
+            
+            $player2 = dealCards($deck, 10);
+            var_dump($player2);
                 
         ?>
 
--- week_3/Day_2/3_hard.php
+++ week_3/Day_2/3_hard.php
@@ -6,92 +6,171 @@
     <p>
 
         <?php
-            /*
+           /*
              * Bring in your createDeck and dealCards function
                For the specified number of players below, assign each one an even set of cards.
                We will do this by counting out how many players there are, counting how many cards
                are in the deck. then dividing them so we know how many cards each player should get
              */
-             function createDeck() {
-                $deck = array();
-                $suits = array ("clubs", "diamonds", "hearts", "spades");
-                $faces = array (
-                    "Ace" => 1, "2" => 2,"3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7,
-                    "8" => 8, "9" => 9, "10" => 10, "Jack" => 11, "Queen" => 12, "King" => 13
-                    );
-                    
-                foreach($suits as $suit){
-                    foreach($faces as $face => $value){
-                        $deck[] = array($suit => array($face => $value));
-                    }
-                }
-                shuffle($deck);
-                return $deck;
-              }
-              
-             function dealCards(&$deck, $number_of_cards = 0) {
-                $setOfCards;
-                $i = 0;
-                $count = 0;
-                while($count != $number_of_cards){
-                    $setOfCards[] = $deck[$i];
-                    array_shift($deck);
-                    $count++;
-                }
-                return $setOfCards;
-            }
-
-               $deck = createDeck();
-               $num_players = 4;
-               $num_cards_in_deck = count($deck);//find a function to count the # of elements in an array
-               $num_cards_to_give_each_player = divCards($num_cards_in_deck, $num_players);
-
-                
-                //  use a for loop to add the "dealt hands" to the $players array
-                
-                  $players = array(); 
-                 
-                 
-                 function divCards($deck_count, $player_count){
-                 
-                 if($deck_count % $player_count !== 0){
-                     $new_deck_count = $deck_count - ($deck_count % $player_count);
-                     return ($new_deck_count / $player_count);
-                 } else {
-                     return ($deck_count / $player_count);
-                 }
-             }
             
-               $deck = createDeck();
+            // creating the deck
+            function createDeck() {
+                $suits = array ("clubs", "diamonds", "hearts", "spades");
+            $faces = array (
+                "Ace" => 1, "2" => 2,"3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7,
+                "8" => 8, "9" => 9, "10" => 10, "Jack" => 11, "Queen" => 12, "King" => 13
+            );
+            
+            $deck = array();
+              
+          
+            foreach($suits as $suit){
+                foreach($faces as $value=>$face){
+                    $newKey = "$value of $suit";
+                    $deck[$newKey] = $face;
+                    
+                }
+            }
+                return $deck;
+                
+            }
+           //shuffle the duck
+            function shuffle_assoc(&$array) {
+                 $keys = array_keys($array);
+                 shuffle($keys);
+            
+             foreach($keys as $key) {
+                 $new[$key] = $array[$key];
+            }
+                 $array = $new;
+                return true;
+            }
+      
+            $deck = createDeck();
+            shuffle_assoc($deck); 
+        
+              
+            /*
+                We will now create a function to deal these cards to each user
+                modify this function so that it returns the number of cards specified to the user
+                also, it must modify the deck so that those cards are no longer available to be ditributed
+            */
+                
                $num_players = 4;
-               $num_cards_in_deck = count($deck);//find a function to count the # of elements in an array
-               $num_cards_to_give_each_player = divCards($num_cards_in_deck, $num_players);
+               $num_cards_in_deck = count($deck);
+               $num_cards_to_give_each_player = $num_cards_in_deck / $num_players;
+                /*
+                  use a for loop to add the "dealt hands" to the $players array
+                */
                 
-                  // use a for loop to add the "dealt hands" to the $players array
-                
-                  $players = array(); 
-                   for($i = 0; $i < $num_players; $i++) {
-                       $players[] = dealCards($deck, $num_cards_to_give_each_player);
-                       
-                   }
-                   
+                //create array of players and their values, then set all values in array equal to the dealCards function
+                 $players = array (
+                        "Player 1" => 1,
+                        "Player 2" => 2,
+                        "Player 3" => 3,
+                        "player 4" => 4);
+               
+           
+            function dealCards($deck, $num_cards_to_give_each_player) {
+                $players = array_chunk($deck, $num_cards_to_give_each_player);
+                return($players);
+            }
+            
+            //call dealCards on all players
+            $players = dealCards($deck, $num_cards_to_give_each_player);
+            
+          
                /*
                lets create a simple game
                each player will play a card and whoever has the highest value wins. if there are 2 cards played
                that have the same value everybody loses and that round is now a draw.
-
                store the results of each game in round_winners and also who won that round as the value.
                if the round is a draw store the value as DRAW
-
                 use a loop to play each game until all oponents are out of cards
-
                 Print out the array of all the rounds. if there was a draw the round should say DRAW
                 if a player has one it should display "Player X" where X is the index of the player
                 
                */
-
-              $round_winners = 
-
+               //score set to 0
+              $p1win = $p2win = $p3win = $p4win = 0;
+              
+               $rounds = 0;
+              
+                
+                
+                        // go through 12 rounds, deal a random card to each player, whoever has the highest value card wins as long as there are no duplicate cards/values
+                  for($rounds = 1; $rounds <= 12; $rounds++){
+                        $player_1_card = $players[0][$rounds];
+                        $player_2_card = $players[1][$rounds];
+                        $player_3_card = $players[2][$rounds];
+                        $player_4_card = $players[3][$rounds];
+                        
+                    //use max function to find highest value card out of 4 players
+                        $round_determine = max($player_1_card,$player_2_card,$player_3_card, $player_4_card);
+                       
+                        if($player_1_card == $player_2_card || $player_1_card == $player_3_card || $player_1_card == $player_4_card || $player_2_card == $player_3_card || $player_2_card == $player_4_card || $player_3_card == $player_4_card){
+                            $won = "DRAW";
+                            }
+                        else{
+                            if($round_determine == $player_1_card){
+                                $won = "Player 1";
+                                $p1win+=1;
+                            
+                            }
+                            elseif($round_determine == $player_2_card){
+                                $p2win += 1;
+                                $won = "Player 2";
+                               
+                            }
+                            elseif($round_determine == $player_3_card){
+                                $p3win +=1;
+                                $won = "Player 3";
+                                
+                            }
+                            elseif($round_determine == $player_4_card){
+                                
+                                $p4win +=1;
+                                $won = "Player 4";
+                                 
+                            }
+                        }
+                     
+                        //scoreboard to show each players value and their wins
+                        echo "<pre>
+                            Game $rounds <br/>
+                            Player 1: $player_1_card Wins: $p1win <br/>
+                            Player 2: $player_2_card Wins: $p2win <br/>
+                            Player 3: $player_3_card Wins: $p3win <br/>
+                            Player 4: $player_4_card Wins: $p4win <br/>
+                       </pre>";
+                        if($won === "DRAW"){
+                            echo "DRAW! <br/>";
+                        }
+                        else{
+                            echo "$won  won this round! <br/>";
+                        }
+                  }
+                 // find the highest value of who has won, whoever is highest is the winnner
+                  $game_winner = max($p1win,$p2win,$p3win,$p4win);
+                  var_dump($game_winner);
+                if($game_winner == $p1win){
+                            echo "Player 1 wins the game!";
+                        }
+                        elseif($game_winner == $p2win){
+                            echo "Player 2 wins the game!";
+                        }
+                        elseif($game_winner == $p3win){
+                            echo "Player 3 wins the game!";
+                        }
+                        elseif($game_winner == $p4win){
+                            echo "Player 4 wins the game!";
+                        }
+              
+                
+                        
+                       
+                  
+               
         ?>
 
     </p>
